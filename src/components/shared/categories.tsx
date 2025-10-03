d58ed5756c04003081f8a28ad3cc9d7f09a2f910
Explorarthecreator <fredadejiemma99@gmail.com>
2025-10-03 13:16:18 +0100

    fix: fix group hover issueS

--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 const Categories = () => {
   return (
     <div className="mt-20 grid grid-cols-1 gap-[84px] md:gap-2.5 md:grid-cols-3 lg:gap-[30px] max-w-[1100px] m-auto">
-      <div className=" h-[165px] rounded-xl w-full bg-[#F1F1F1] uppercase relative flex justify-center items-end pb-5">
+      <Link
+        href={"/headphones"}
+        className=" h-[165px] rounded-xl w-full group bg-[#F1F1F1] uppercase relative flex justify-center items-end pb-5"
+      >
         <div className="  w-24 h-24  m-auto absolute -top-16 ">
           <Image
             src={"/assets/headphones.svg"}
@@ -24,17 +27,19 @@ const Categories = () => {
         </div>
         <div className="space-y-4">
           <h1 className="text-center text-[15px] font-bold">Headphones</h1>
-          <Link
-            href={"/headphones"}
-            className="font-bold text-[13px] flex items-center gap-1 justify-center hover:text-[#D87D4A]  opacity-50 hover:opacity-100"
-          >
-            <span>shop</span>
+          <div className="font-bold text-[13px] flex items-center gap-1 justify-center ">
+            <span className="group-hover:text-[#D87D4A]  opacity-50 group-hover:opacity-100">
+              shop
+            </span>
             <ArrowRight2 size={12} color="#D87D4A" className="" />
-          </Link>
+          </div>
         </div>
-      </div>
+      </Link>
 
-      <div className=" h-[165px] rounded-xl w-full bg-[#F1F1F1] uppercase relative flex justify-center items-end pb-5">
+      <Link
+        href={"/speakers"}
+        className=" h-[165px] rounded-xl w-full bg-[#F1F1F1] group uppercase relative flex justify-center items-end pb-5"
+      >
         <div className="  w-24 h-24  m-auto absolute -top-16 ">
           <Image
             src={"/assets/speaker.svg"}
@@ -53,17 +58,19 @@ const Categories = () => {
         </div>
         <div className="space-y-4">
           <h1 className="text-center text-[15px] font-bold">Speakers</h1>
-          <Link
-            href={"/speakers"}
-            className="font-bold text-[13px] flex items-center gap-1 justify-center hover:text-[#D87D4A]  opacity-50 hover:opacity-100"
-          >
-            <span>shop</span>
+          <div className="font-bold text-[13px] flex items-center gap-1 justify-center ">
+            <span className="group-hover:text-[#D87D4A]  opacity-50 group-hover:opacity-100">
+              shop
+            </span>
             <ArrowRight2 size={12} color="#D87D4A" className="" />
-          </Link>
+          </div>
         </div>
-      </div>
+      </Link>
 
-      <div className=" h-[165px] rounded-xl w-full bg-[#F1F1F1] uppercase relative flex justify-center items-end pb-5">
+      <Link
+        href={"/earphones"}
+        className=" h-[165px] rounded-xl w-full bg-[#F1F1F1] uppercase relative flex justify-center items-end pb-5 group"
+      >
         <div className="  w-24 h-24  m-auto absolute -top-16 ">
           <Image
             src={"/assets/earphone.svg"}
@@ -82,15 +89,14 @@ const Categories = () => {
         </div>
         <div className="space-y-4">
           <h1 className="text-center text-[15px] font-bold">Earphones</h1>
-          <Link
-            href={"/earphones"}
-            className="font-bold text-[13px] flex items-center gap-1 justify-center hover:text-[#D87D4A] opacity-50 hover:opacity-100"
-          >
-            <span>shop</span>
+          <div className="font-bold text-[13px] flex items-center gap-1 justify-center ">
+            <span className="opacity-50 group-hover:text-[#D87D4A]  group-hover:opacity-100">
+              shop
+            </span>
             <ArrowRight2 size={14} color="#D87D4A" />
-          </Link>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
